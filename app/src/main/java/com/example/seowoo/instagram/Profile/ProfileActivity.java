a2f72853768d3aeec7ef1f1adff76a7de877e098
양서우 <alsmwsk@naver.com>
2018-06-12 13:48:25 +0900

    changed

--- app/src/main/java/com/example/seowoo/instagram/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/seowoo/instagram/Profile/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import com.example.seowoo.instagram.R;
 import com.example.seowoo.instagram.Utils.BottomNavigationViewHelper;
@@ -21,10 +22,29 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: starting");
 
-        setupBottomNavigationView();
+        //setupBottomNavigationView();
+        setupToolbar();
+    }
+
+    private void setupToolbar(){
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItemClick: " + item);
+                
+                switch (item.getItemId()){
+                    case R.id.profileMenu:
+                        Log.d(TAG, "onMenuItemClick: Navigating to Profile Preference");
+                }
+                return false;
+            }
+        });
     }
 
     /** BottomNavigationView setup common layout for all activity**/
@@ -36,5 +56,11 @@ public class ProfileActivity extends AppCompatActivity {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }
